d8cd2a28bb4a34affbea919e29ec76e12012f7ac
caleb <cakpomughe@gmail.com>
2025-02-12 21:10:48 +0100

    render port fix

--- fastapi-book-project/fastapi-book-project/main.py
+++ fastapi-book-project/fastapi-book-project/main.py
@@ -1,3 +1,5 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -21,3 +23,7 @@ app.include_router(api_router, prefix=settings.API_PREFIX)
 async def health_check():
     """Checks if server is active."""
     return {"status": "active"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  
+    uvicorn.run(app, host="0.0.0.0", port=port)
